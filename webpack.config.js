b6a4c9ee319f99f567500023f8b165cbda962125
Mike Gowen <mike@mikegowen.com>
2021-01-01 23:19:25 -0800

    First!

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require("path")
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ],
+  devServer: {
+    publicPath: "/"
+  },
+  entry: "./src/client/index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "[name].[fullhash].js"
+  }
+}
